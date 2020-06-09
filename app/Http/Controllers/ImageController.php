c0fdfa4887a126971eadd644cca7bcc07b3d20e0
darehamed <42046888+darehamed@users.noreply.github.com>
2020-06-09 14:54:55 +0100

    Added the Intervention package and implemented it to resize images

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Image;
+use Image;
 
 class ImageController extends Controller
 {
@@ -15,6 +17,28 @@ class ImageController extends Controller
             'height' => 'bail|required',
             'width' => 'bail|required'
             ]);
+
+
+        # Code to resize images - implements Intervention
+
+        $image = $request->file('image');
+        $filename = $image->getClientOriginalName();
+
+        $image->move(public_path('thumbnail/'), $filename);
+
+        $image_resize = Image::make(public_path('thumbnail/' . $filename));
+        $image_resize->fit($request->get('width'), $request->get('height'));
+        $image_resize->save(public_path('thumbnail/') .$filename);
+
+        # uncomment below to save image to database
+        /* $img = new Image();
+        $img->email = $request->email;
+        $img->image = $filename;
+        $img->save(); */
+        # Code to resize images - implements Intervention
+
+
+
 
         return response()->json([
             'message'  => 'resize method hit'
